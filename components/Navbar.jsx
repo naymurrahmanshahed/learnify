69b01fa02bcff66ffea54aff5b4ad0d9853c198e
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-05-09 22:47:43 +0600

    navbar added

---
+++ components/Navbar.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div className="h-20 bg-black text-gray-400  flex items-center">
+      <div className=" wrapper flex justify-between ">
+        <Link href="/" className="text-white font-semibold">
+          Cordemy
+        </Link>
+        <div className="flex gap-5">
+          <Link href="/" className="hover:text-white transition-colors">
+            Home
+          </Link>
+          <Link href="/courses" className="hover:text-white transition-colors">
+            Courses
+          </Link>
+          <Link href="/about" className="hover:text-white transition-colors">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-white transition-colors">
+            Contact
+          </Link>
+        </div>
+        <div>
+          <Link href="/login">Sign in </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
