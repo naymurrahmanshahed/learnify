168cac65d52c6b2c253cb7343b62eddaa4183e60
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-05-12 19:48:50 +0600

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div className="">Footer</div>;
+  return (
+    <div className="footer bg-black text-gray-400 text-center py-10">
+      <p>&copy; {new Date().getFullYear()}, Learnify. All rights reserved. </p>
+    </div>
+  );
 };
 
 export default Footer;
